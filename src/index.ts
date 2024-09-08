fc2340afd0e61b98d984f60910d3f487992198de
jhyunwoo <jhyunwoo0228@gmail.com>
2024-09-09 08:32:19 +0900

    change url

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,28 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+async function expiringShares(){
+	const expireShares = await fetch('https://beta.moveto.kr/api/admin/storage')
+	const result = await expireShares.json()
+	return {expireShares, result}
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const {expireShares, result} = await expiringShares()
+
+		if(expireShares.ok){
+			return new Response(JSON.stringify(result), { status: 200 })
+		}else{
+			return new Response(JSON.stringify(result), { status: 500 })
+		}
+	},
+
+	async scheduled(
+		controller: ScheduledController,
+		env: Env,
+		ctx: ExecutionContext,
+	) {
+		await expiringShares()
 	},
 } satisfies ExportedHandler<Env>;
